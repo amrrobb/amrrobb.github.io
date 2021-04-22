aaca91aeb3ea660b5beed751603a1e733b14f365
Muhamad Hilman <muhamadhilman885@gmail.com>
2021-04-22 21:46:30 +0700

    Add logo CakePedia

---
+++ index.js
@@ -0,0 +1,14 @@
+// let listCake = [
+//     {
+//         name: 'Brownies Keju',
+//         type: 'Brownies',
+//         price: 50_000,
+//         image: 'src asetnya'
+//     },
+//     {
+//         name: 'Bolu Karamel',
+//         type: 'Bolu',
+//         price: 45_000,
+//         image: 'src asetnya'
+//     }
+// ]
